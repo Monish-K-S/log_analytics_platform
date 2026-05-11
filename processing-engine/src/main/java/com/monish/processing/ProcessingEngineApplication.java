aff33cdf059ca68c999a55a986174ef143453031
Monish-K-S <k6mg5343@gmail.com>
2026-05-11 19:30:44 +0530

    added feature to analyse metrics by aggregating service and level counts from incoming log stream

--- processing-engine/src/main/java/com/monish/processing/ProcessingEngineApplication.java
+++ processing-engine/src/main/java/com/monish/processing/ProcessingEngineApplication.java
@@ -3,9 +3,11 @@ package com.monish.processing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
+@EnableScheduling
 public class ProcessingEngineApplication {
 
     public static void main(String[] args) {
--- processing-engine/src/main/java/com/monish/processing/analytics/LogAnalyticsService.java
+++ processing-engine/src/main/java/com/monish/processing/analytics/LogAnalyticsService.java
@@ -3,18 +3,32 @@ package com.monish.processing.analytics;
 import com.monish.common.model.LogEvent;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogAnalyticsService {
 
     private final Map<String, Integer> levelCounts = new HashMap<>();
+    private final Map<String, Integer> serviceCounts = new HashMap<>();
 
     public void processLog(LogEvent logEvent) {
         levelCounts.put(
             logEvent.getLevel(),
             levelCounts.getOrDefault(logEvent.getLevel(), 0) + 1
         );
+        serviceCounts.put(
+            logEvent.getService(),
+            serviceCounts.getOrDefault(logEvent.getService(), 0) + 1
+        );
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void printAnalytics() {
+        System.out.println("===== Analytics Snapshot =====");
+        System.out.println(" Level Counts:   ");
         System.out.println(levelCounts);
+        System.out.println(" Service Counts:   ");
+        System.out.println(serviceCounts);
     }
 }
